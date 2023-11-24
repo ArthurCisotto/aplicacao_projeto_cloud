af3aaf28a55d071f5ff0bdbe55f349398ebb0da1
ArthurCisotto <arthurcisottom@gmail.com>
2023-11-24 20:50:22 -0300

    volta

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-import logging
 
 app = FastAPI()
 
@@ -11,14 +10,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
-logging.basicConfig(level=logging.INFO, filename='app.log', 
-                    format='%(asctime)s %(levelname)s:%(message)s')
-logger = logging.getLogger(__name__)
-
-@app.get("/hello")
-def read_root():
-    return {"Hello": "World"}
-'''
 class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True, index=True)
@@ -31,29 +22,19 @@ Base.metadata.create_all(bind=engine)
 
 @app.post("/items/")
 def create_item(name: str, description: str):
-    try:
-        db = SessionLocal()
-        new_item = Item(name=name, description=description)
-        db.add(new_item)
-        db.commit()
-        db.refresh(new_item)
-        logger.info(f"Item created with ID: {new_item.id}")
-        return new_item
-    except Exception as e:
-        logger.error(f"Error creating item: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+    db = SessionLocal()
+    new_item = Item(name=name, description=description)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    try:
-        db = SessionLocal()
-        item = db.query(Item).filter(Item.id == item_id).first()
-        if item is None:
-            logger.warning(f"Item with ID {item_id} not found")
-            raise HTTPException(status_code=404, detail="Item not found")
-        logger.info(f"Item retrieved with ID: {item_id}")
-        return item
-    except Exception as e:
-        logger.error(f"Error retrieving item: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
-'''
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+# Implementar métodos PUT e DELETE conforme necessário
