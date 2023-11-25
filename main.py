2c448cd54d1fda01d3f7d0e44cb0e8fae6dca5a4
ArthurCisotto <arthurcisottom@gmail.com>
2023-11-24 22:41:21 -0300

    Remove log

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-import boto3
-from datetime import datetime
+
 
 app = FastAPI()
 
@@ -21,27 +20,6 @@ SessionLocal = sessionmaker(bind=engine)
 
 Base.metadata.create_all(bind=engine)
 
-# Configuração do cliente do CloudWatch
-cloudwatch = boto3.client('cloudwatch', region_name='us-east-1')
-
-def send_custom_metric(metric_name, value):
-    cloudwatch.put_metric_data(
-        MetricData=[
-            {
-                'MetricName': metric_name,
-                'Dimensions': [
-                    {
-                        'Name': 'Application',
-                        'Value': 'MyFastAPIApp'
-                    },
-                ],
-                'Unit': 'Count',
-                'Value': value
-            },
-        ],
-        Namespace='MyAppMetrics'
-    )
-
 @app.post("/items/")
 def create_item(name: str, description: str):
     db = SessionLocal()
@@ -49,14 +27,12 @@ def create_item(name: str, description: str):
     db.add(new_item)
     db.commit()
     db.refresh(new_item)
-    send_custom_metric('ItemsCreated', 1)
     return new_item
 
 @app.get("/items/")
 def read_items():
     db = SessionLocal()
     items = db.query(Item).all()
-    send_custom_metric('ItemsRead', len(items))
     return items
 
 @app.get("/items/{item_id}")
@@ -65,7 +41,6 @@ def read_item(item_id: int):
     item = db.query(Item).filter(Item.id == item_id).first()
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    send_custom_metric('ItemsRead', 1)
     return item
 
 @app.put("/items/{item_id}")
@@ -78,7 +53,6 @@ def update_item(item_id: int, name: str, description: str):
     item.name = name
     item.description = description
     db.commit()
-    send_custom_metric('ItemsUpdated', 1)
     return item
 
 @app.delete("/items/{item_id}")
@@ -90,6 +64,5 @@ def delete_item(item_id: int):
 
     db.delete(item)
     db.commit()
-    send_custom_metric('ItemsDeleted', 1)
     return {"detail": "Item deleted successfully"}
 
